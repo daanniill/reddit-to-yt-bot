4e5caccb5f43e9d059bf43c76f7132bcbb4f2201
daanniill <daniil.malakhov12@gmail.com>
2025-07-02 13:20:07 -0700

    made script to download vid using yt_dlp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import praw
 import os
+import yt_dlp
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -15,4 +17,14 @@ def get_reddit_post():
         if 'v.redd.it' in post.url:
             return post.title, post.url
     return None, None
-    
+
+def download_video(url, filename='video.mp4'):
+    ydl_opts = {
+        'outtmpl': filename,
+        'format': 'bv+ba/best',
+        'merge_output_format': 'mp4',
+        'quiet': True,
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
